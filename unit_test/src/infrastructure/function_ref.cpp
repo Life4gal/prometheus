c781f783fe3ad3dd5dd99062986b140a2282086a
life4gal <life4gal@gmail.com>
2023-08-04 16:09:56 +0800

    Test cases for function_ref.

---
+++ unit_test/src/infrastructure/function_ref.cpp
@@ -0,0 +1,114 @@
+#include <boost/ut.hpp>
+#include <prometheus/infrastructure/function_ref.hpp>
+#include <prometheus/macro.hpp>
+
+using namespace boost::ut;
+using namespace gal::prometheus;
+
+namespace
+{
+	GAL_PROMETHEUS_NO_DESTROY suite test_function_ref = []
+	{
+		"functor"_test = []
+		{
+			struct functor
+			{
+				constexpr auto operator()(const int a, const int b) const noexcept -> int { return a + b; }
+
+				constexpr auto operator()(int& a) const noexcept -> void { a = 42; }
+			};
+
+			functor f{};
+
+			FunctionRef<int(int, int)> a{f};
+			expect((a(42, 1337) == _i{42 + 1337}) >> fatal);
+
+			FunctionRef<int(int, short)> b{f};
+			expect((b(42, 1337) == _i{42 + 1337}) >> fatal);
+
+			int v = 1337;
+			expect((v == 1337_i) >> fatal);
+
+			FunctionRef<void(int&)> c{f};
+			c(v);
+			expect((v == 42_i) >> fatal);
+
+			v = 1337;
+			FunctionRef<void(int&, int)> d{f};
+			// note: call functor::operator()(const int a, const int b)
+			d(v, 123);
+			expect((v == 1337_i) >> fatal);
+		};
+
+		"function pointer"_test = []
+		{
+			const auto f = +[](const int a, const int b) noexcept -> int { return a + b; };
+
+			FunctionRef<int(int, int)> a{f};
+			expect((a(42, 1337) == _i{42 + 1337}) >> fatal);
+
+			FunctionRef<int(int, short)> b{f};
+			expect((b(42, 1337) == _i{42 + 1337}) >> fatal);
+
+			// compatible
+			FunctionRef<void(int, short)> c{f};
+			c(42, 1337);
+		};
+
+		"lambda"_test = []
+		{
+			{
+				const auto f = [](const int a, const int b) noexcept -> int { return a + b; };
+
+				FunctionRef<int(int, int)> a{f};
+				expect((a(42, 1337) == _i{42 + 1337}) >> fatal);
+
+				FunctionRef<int(int, short)> b{f};
+				expect((b(42, 1337) == _i{42 + 1337}) >> fatal);
+
+				// compatible
+				FunctionRef<void(int, short)> c{f};
+				c(42, 1337);
+			}
+			{
+				int        i = 42;
+				const auto f = [&i](const int a, const int b) noexcept -> int { return i + a + b; };
+
+				FunctionRef<int(int, int)> a{f};
+				expect((a(42, 1337) == _i{i + 42 + 1337}) >> fatal);
+
+				FunctionRef<int(int, short)> b{f};
+				expect((b(42, 1337) == _i{i + 42 + 1337}) >> fatal);
+
+				// compatible
+				FunctionRef<void(int, short)> c{f};
+				c(42, 1337);
+			}
+		};
+
+		"member function"_test = []
+		{
+			class Foo
+			{
+				int _{0};
+
+			public:
+				[[nodiscard]] constexpr auto bar(const int a, const int b) noexcept -> int// NOLINT
+				{
+					_ = a + b;
+
+					return _;
+				}
+			};
+
+			Foo foo{};
+
+			FunctionRef<int(Foo&, int, int)> a{[](Foo& f, const int v1, const int v2) noexcept -> int { return f.bar(v1, v2); }};
+			expect((a(foo, 42, 1337) == _i{42 + 1337}) >> fatal);
+
+			auto                             function_pointer = std::mem_fn(&Foo::bar);
+			FunctionRef<int(Foo&, int, int)> b{function_pointer};
+			expect((b(foo, 42, 1337) == _i{42 + 1337}) >> fatal);
+		};
+	};
+}
