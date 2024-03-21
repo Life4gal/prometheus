8b3d2f166b7b7bfd603c806e014fcdd67aebfd05
life4gal <life4gal@gmail.com>
2024-03-21 13:52:50 +0800

    fix: meta.name is no longer available only for MSVC.

--- src/meta/name.hpp
+++ src/meta/name.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <string_view>
 
 struct dummy_struct_do_not_put_into_any_namespace {};
 
@@ -13,25 +14,27 @@ namespace gal::prometheus::meta
 {
 	namespace name
 	{
+		#if defined(GAL_PROMETHEUS_COMPILER_MSVC)
 		template<typename>
-		[[nodiscard]] constexpr auto get_full_function_name() noexcept -> std::string_view
-		{
-			#if defined(GAL_PROMETHEUS_COMPILER_MSVC)
-			return {__FUNCSIG__, sizeof(__FUNCSIG__)};
-			#else
-			return {__PRETTY_FUNCTION__, sizeof(__PRETTY_FUNCTION__)};
-			#endif
-		}
+		[[nodiscard]] constexpr auto get_full_function_name() noexcept -> std::string_view { return {__FUNCSIG__, sizeof(__FUNCSIG__)}; }
 
 		template<auto>
-		[[nodiscard]] constexpr auto get_full_function_name() noexcept -> std::string_view
-		{
-			#if defined(GAL_PROMETHEUS_COMPILER_MSVC)
-			return {__FUNCSIG__, sizeof(__FUNCSIG__)};
-			#else
-			return {__PRETTY_FUNCTION__, sizeof(__PRETTY_FUNCTION__)};
-			#endif
-		}
+		[[nodiscard]] constexpr auto get_full_function_name() noexcept -> std::string_view { return {__FUNCSIG__, sizeof(__FUNCSIG__)}; }
+
+		#elif defined(GAL_PROMETHEUS_COMPILER_CLANG) or defined(GAL_PROMETHEUS_COMPILER_CLANG_CL)
+		template<typename T>// DO NOT REMOVE `T`
+		[[nodiscard]] constexpr auto get_full_function_name() noexcept -> std::string_view { return {__PRETTY_FUNCTION__, sizeof(__PRETTY_FUNCTION__)}; }
+
+		template<auto V>// DO NOT REMOVE `V`
+		[[nodiscard]] constexpr auto get_full_function_name() noexcept -> std::string_view { return {__PRETTY_FUNCTION__, sizeof(__PRETTY_FUNCTION__)}; }
+
+		#else
+		template<typename>
+		[[nodiscard]] constexpr auto get_full_function_name() noexcept -> std::string_view { return {__PRETTY_FUNCTION__, sizeof(__PRETTY_FUNCTION__)}; }
+
+		template<auto>
+		[[nodiscard]] constexpr auto get_full_function_name() noexcept -> std::string_view { return {__PRETTY_FUNCTION__, sizeof(__PRETTY_FUNCTION__)}; }
+		#endif
 	}
 
 	GAL_PROMETHEUS_MODULE_EXPORT_BEGIN
@@ -39,24 +42,52 @@ namespace gal::prometheus::meta
 	template<typename T>
 	[[nodiscard]] constexpr auto name_of_type() noexcept -> std::string_view
 	{
-		// const class std::basic_string_view<char, struct std::char_traits<char>> `__calling_convention` `namespace`::get_full_function_name< `[struct]` `dummy_struct_do_not_put_into_any_namespace` >( `[void]` )
+		#if defined(GAL_PROMETHEUS_COMPILER_MSVC)
+		// MSVC
+		// const class std::basic_string_view<char, struct std::char_traits<char>> `__calling_convention` `namespace`::get_full_function_name<struct `dummy_struct_do_not_put_into_any_namespace` >(void)
 		constexpr std::string_view full_function_name      = name::get_full_function_name<dummy_struct_do_not_put_into_any_namespace>();
 		constexpr auto             full_function_name_size = full_function_name.size();
 
-		// `[struct]` `dummy_struct_do_not_put_into_any_namespace`
-		constexpr std::string_view dummy_struct_name =
-				#if defined(GAL_PROMETHEUS_COMPILER_MSVC)
-				"struct "
-				#endif
-				"dummy_struct_do_not_put_into_any_namespace";
+		constexpr std::string_view dummy_struct_name{"struct dummy_struct_do_not_put_into_any_namespace"};
 		constexpr auto dummy_struct_name_size = std::ranges::size(dummy_struct_name);
 
 		// const class std::basic_string_view<char, struct std::char_traits<char>> `__calling_convention` `namespace`::get_full_function_name<
 		constexpr auto full_function_name_prefix_size = full_function_name.find(dummy_struct_name);
 		static_assert(full_function_name_prefix_size != std::string_view::npos);
 
-		// >( `[void]` )
+		// >(void)
 		constexpr auto full_function_name_suffix_size = full_function_name_size - full_function_name_prefix_size - dummy_struct_name_size;
+		#elif defined(GAL_PROMETHEUS_COMPILER_CLANG) or defined(GAL_PROMETHEUS_COMPILER_CLANG_CL)
+		// CLANG/CLANG-CL
+		// std::string_view `namespace`::get_full_function_name() [T = `dummy_struct_do_not_put_into_any_namespace`]
+		constexpr std::string_view full_function_name      = name::get_full_function_name<dummy_struct_do_not_put_into_any_namespace>();
+		constexpr auto             full_function_name_size = full_function_name.size();
+
+		constexpr std::string_view dummy_struct_name{"dummy_struct_do_not_put_into_any_namespace"};
+		constexpr auto             dummy_struct_name_size = std::ranges::size(dummy_struct_name);
+
+		// std::string_view `namespace`::get_full_function_name() [T =
+		constexpr auto full_function_name_prefix_size = full_function_name.find(dummy_struct_name);
+		static_assert(full_function_name_prefix_size != std::string_view::npos);
+
+		// ]
+		constexpr auto full_function_name_suffix_size = full_function_name_size - full_function_name_prefix_size - dummy_struct_name_size;
+		#else
+		// GCC
+		// constexpr std::string_view `namespace`::get_full_function_name() [with <template-parameter-1-1> = `dummy_struct_do_not_put_into_any_namespace`; std::string_view = std::basic_string_view<char>]
+		constexpr std::string_view full_function_name      = name::get_full_function_name<dummy_struct_do_not_put_into_any_namespace>();
+		constexpr auto             full_function_name_size = full_function_name.size();
+
+		constexpr std::string_view dummy_struct_name{"dummy_struct_do_not_put_into_any_namespace"};
+		constexpr auto             dummy_struct_name_size = std::ranges::size(dummy_struct_name);
+
+		// constexpr std::string_view `namespace`::get_full_function_name() [with <template-parameter-1-1> =
+		constexpr auto full_function_name_prefix_size = full_function_name.find(dummy_struct_name);
+		static_assert(full_function_name_prefix_size != std::string_view::npos);
+
+		// ; std::string_view = std::basic_string_view<char>]
+		constexpr auto full_function_name_suffix_size = full_function_name_size - full_function_name_prefix_size - dummy_struct_name_size;
+		#endif
 
 		auto full_name = name::get_full_function_name<std::decay_t<T>>();
 		full_name.remove_prefix(full_function_name_prefix_size);
@@ -71,7 +102,9 @@ namespace gal::prometheus::meta
 		requires std::is_enum_v<EnumType>
 	[[nodiscard]] constexpr auto name_of_enum_value() noexcept -> std::string_view
 	{
-		// class std::basic_string_view<char,struct std::char_traits<char> > `__calling_convention` `namespace`::get_full_function_name<`DummyEnumDoNotPutIntoAnyNamespace::DO_NOT_USE`>( `[void]` ) noexcept
+		#if defined(GAL_PROMETHEUS_COMPILER_MSVC)
+		// MSVC
+		// class std::basic_string_view<char,struct std::char_traits<char> > `__calling_convention` `namespace`::get_full_function_name<`DummyEnumDoNotPutIntoAnyNamespace::DO_NOT_USE`>(void) noexcept
 		constexpr std::string_view full_function_name      = name::get_full_function_name<DummyEnumDoNotPutIntoAnyNamespace::DO_NOT_USE>();
 		constexpr auto             full_function_name_size = full_function_name.size();
 
@@ -82,8 +115,39 @@ namespace gal::prometheus::meta
 		constexpr auto full_function_name_prefix_size = full_function_name.find(dummy_enum_value_name);
 		static_assert(full_function_name_prefix_size != std::string_view::npos);
 
-		// >( `[void]` ) noexcept
+		// >(void) noexcept
 		constexpr auto full_function_name_suffix_size = full_function_name_size - full_function_name_prefix_size - dummy_enum_value_name_size;
+		#elif defined(GAL_PROMETHEUS_COMPILER_CLANG) or defined(GAL_PROMETHEUS_COMPILER_CLANG_CL)
+		// CLANG/CLANG-CL
+		// std::string_view `namespace`::get_full_function_name() [V = `DummyEnumDoNotPutIntoAnyNamespace::DO_NOT_USE`]
+		constexpr std::string_view full_function_name      = name::get_full_function_name<DummyEnumDoNotPutIntoAnyNamespace::DO_NOT_USE>();
+		constexpr auto             full_function_name_size = full_function_name.size();
+
+		constexpr std::string_view dummy_struct_name{"DummyEnumDoNotPutIntoAnyNamespace::DO_NOT_USE"};
+		constexpr auto             dummy_struct_name_size = std::ranges::size(dummy_struct_name);
+
+		// std::string_view `namespace`::get_full_function_name() [V =
+		constexpr auto full_function_name_prefix_size = full_function_name.find(dummy_struct_name);
+		static_assert(full_function_name_prefix_size != std::string_view::npos);
+
+		// ]
+		constexpr auto full_function_name_suffix_size = full_function_name_size - full_function_name_prefix_size - dummy_struct_name_size;
+		#else
+		// GCC
+		// constexpr std::string_view `namespace`::get_full_function_name() [with auto <anonymous> = `DummyEnumDoNotPutIntoAnyNamespace::DO_NOT_USE`; std::string_view = std::basic_string_view<char>]
+		constexpr std::string_view full_function_name      = name::get_full_function_name<DummyEnumDoNotPutIntoAnyNamespace::DO_NOT_USE>();
+		constexpr auto             full_function_name_size = full_function_name.size();
+
+		constexpr std::string_view dummy_struct_name{"DummyEnumDoNotPutIntoAnyNamespace::DO_NOT_USE"};
+		constexpr auto             dummy_struct_name_size = std::ranges::size(dummy_struct_name);
+
+		// std::string_view `namespace`::get_full_function_name() [V =
+		constexpr auto full_function_name_prefix_size = full_function_name.find(dummy_struct_name);
+		static_assert(full_function_name_prefix_size != std::string_view::npos);
+
+		// ; std::string_view = std::basic_string_view<char>]
+		constexpr auto full_function_name_suffix_size = full_function_name_size - full_function_name_prefix_size - dummy_struct_name_size;
+		#endif
 
 		auto full_name = name::get_full_function_name<EnumValue>();
 		full_name.remove_prefix(full_function_name_prefix_size);
