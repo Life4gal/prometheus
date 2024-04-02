11c819acf706db2d4fcc8d04e8365f16643b0004
life4gal <life4gal@gmail.com>
2024-04-02 14:48:23 +0800

    `feat`: Add functional.functor and functional.aligned_union.

--- src/functional/functional.hpp
+++ src/functional/functional.hpp
@@ -15,41 +15,13 @@ export module gal.prometheus.functional;
 import std;
 export import :type_list;
 export import :value_list;
+export import :functor;
+export import :aligned_union;
 
 #else
-#include <type_traits>
-#include <functional>
-
 #include <prometheus/macro.hpp>
 #include <functional/type_list.hpp>
 #include <functional/value_list.hpp>
+#include <functional/functor.hpp>
+#include <functional/aligned_union.hpp>
 #endif
-
-namespace gal::prometheus::functional
-{
-	GAL_PROMETHEUS_MODULE_EXPORT_BEGIN
-
-	template<typename FunctionType>
-	struct y_combinator
-	{
-		using function_type = FunctionType;
-
-		function_type function;
-
-		template<typename... Args>
-		constexpr auto operator()(Args&&... args) const noexcept(std::is_nothrow_invocable_v<function_type, decltype(*this), Args...>) -> decltype(auto)//
-		{
-			// we pass ourselves to function, the lambda should take the first argument as `auto&& self` or similar.
-			return std::invoke(function, *this, std::forward<Args>(args)...);
-		}
-	};
-
-	template<typename... Ts>
-	struct overloaded : Ts...
-	{
-		constexpr explicit overloaded(Ts&&... ts) noexcept((std::is_nothrow_constructible_v<Ts, decltype(ts)> and ...))
-			: Ts{std::forward<Ts>(ts)}... {}
-	};
-
-	GAL_PROMETHEUS_MODULE_EXPORT_END
-}
