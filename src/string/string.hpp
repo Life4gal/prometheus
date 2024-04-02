48a289f3543a29a74cbff379dfb8b6493d05259e
life4gal <life4gal@gmail.com>
2024-04-02 14:19:18 +0800

    `feat`: Add string.wildcard_match.

---
+++ src/string/string.hpp
@@ -0,0 +1,19 @@
+// This file is part of prometheus
+// Copyright (C) 2022-2024 Life4gal <life4gal@gmail.com>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level directory of this distribution.
+
+#pragma once
+
+#if GAL_PROMETHEUS_USE_MODULE
+module;
+
+#include <prometheus/macro.hpp>
+
+export module gal.prometheus.string;
+
+export import :wildcard_match;
+
+#else
+#include <string/wildcard_match.hpp>
+#endif
