9c9b27a53e6920b240d1e23a6d8108e943390a3d
life4gal <life4gal@gmail.com>
2024-07-08 16:21:41 +0800

    `test`: Add chars.utf8.

--- unit_test/src/chars/utf8.cpp
+++ unit_test/src/chars/utf8.cpp
@@ -16,7 +16,7 @@ import gal.prometheus.numeric;
 #include <numeric/numeric.ixx>
 #endif
 
-namespace 
+namespace
 {
 	using namespace gal::prometheus;
 
@@ -24,7 +24,7 @@ namespace
 	[[nodiscard]] auto make_source(auto generator, const std::size_t size) -> std::basic_string<Char>
 	{
 		std::basic_string<Char> source{};
-		for (std::size_t i = 0; i < size; )
+		for (std::size_t i = 0; i < size;)
 		{
 			i += generator(source);
 		}
@@ -32,7 +32,7 @@ namespace
 		return source;
 	}
 
-	GAL_PROMETHEUS_COMPILER_NO_DESTROY unit_test::suite<"chars.utf8_char"> _ = []
+	GAL_PROMETHEUS_COMPILER_NO_DESTROY unit_test::suite<"chars.utf8_char"> chars_utf8_char = []
 	{
 		using namespace unit_test;
 		using namespace literals;
@@ -46,9 +46,20 @@ namespace
 			do
 			{
 				v = random.get<unsigned>(0, 0x0010'ffef);
-			}
-			// skip surrogate pairs
-			while (v >= 0xd800 && v <= 0xdfff); 
+			} while (
+				// skip surrogate pairs
+				(v >= 0xd800 and v <= 0xdfff) // or
+				// // skip non-characters
+				// (v >= 0xfdd0 and v <= 0xfdef) or (v & 0xfffe) == 0xfffe or
+				// // skip control characters
+				// (v <= 0x001f) or (v >= 0x007f and v <= 0x009f) or
+				// // skip special characters
+				// (v >= 0xfff0 and v <= 0xfff8) or
+				// // skip tag characters and variation selector supplement
+				// (v >= 0x000e'0000 and v <= 0x000e'01ef) or
+				// // skip private use areas
+				// (v >= 0x000f'0000)
+			);
 
 			if (v < 0x0080)
 			{
@@ -70,9 +81,9 @@ namespace
 			}
 			else
 			{
-				source.push_back(static_cast<char>(0xf0 | (v >> 18)));	
-				source.push_back(static_cast<char>(0x80 | ((v >> 12) & 0x3f)));	
-				source.push_back(static_cast<char>(0x80 | ((v >> 6) & 0x3f)));	
+				source.push_back(static_cast<char>(0xf0 | (v >> 18)));
+				source.push_back(static_cast<char>(0x80 | ((v >> 12) & 0x3f)));
+				source.push_back(static_cast<char>(0x80 | ((v >> 6) & 0x3f)));
 				source.push_back(static_cast<char>(0x80 | (v & 0x3f)));
 				return 4;
 			}
@@ -91,17 +102,25 @@ namespace
 				using operators::operator==;
 				expect(chars::validate<chars::CharsCategory::UTF8_CHAR>(source) == "valid utf8_char string"_b) << fatal;
 				expect(chars::length<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::ASCII>(source) == value{source.size()}) << fatal;
-				expect((chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::ASCII>(source) == source) == "valid ascii string"_b) << fatal;
-				expect((chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::ASCII, chars::InputProcessPolicy::ASSUME_VALID_INPUT>(source) == source) == "valid ascii string"_b) << fatal;
+
+				const auto dest = chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::ASCII>(source);
+				expect((dest == ref{source}) == "valid ascii string"_b) << fatal;
+
+				const auto dest_a = chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::ASCII, chars::InputProcessPolicy::ASSUME_VALID_INPUT>(source);
+				expect((dest_a == ref{source}) == "valid ascii string"_b) << fatal;
 			};
 
 			"to_utf8_char"_test = [source = make_source<char>(generator, random.get<std::size_t>(std::numeric_limits<char>::max() / 2, std::numeric_limits<char>::max()))]
 			{
 				using operators::operator==;
-				expect(chars::validate<chars::CharsCategory::UTF8_CHAR>(source) == "valid utf8_char string"_b)<< fatal;
+				expect(chars::validate<chars::CharsCategory::UTF8_CHAR>(source) == "valid utf8_char string"_b) << fatal;
 				expect(chars::length<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF8_CHAR>(source) == value{source.size()}) << fatal;
-				expect((chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF8_CHAR>(source) == source) == "valid utf8_char string"_b) << fatal;
-				expect((chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF8_CHAR, chars::InputProcessPolicy::ASSUME_VALID_INPUT>(source) == source) == "valid utf8_char string"_b) << fatal;
+
+				const auto dest = chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF8_CHAR>(source);
+				expect((dest == ref{source}) == "valid utf8_char string"_b) << fatal;
+
+				const auto dest_a = chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF8_CHAR, chars::InputProcessPolicy::ASSUME_VALID_INPUT>(source);
+				expect((dest_a == ref{source}) == "valid utf8_char string"_b) << fatal;
 			};
 
 			"to_utf8"_test = [source = make_source<char>(generator, random.get<std::size_t>(std::numeric_limits<char>::max() / 2, std::numeric_limits<char>::max()))]
@@ -109,36 +128,198 @@ namespace
 				using operators::operator==;
 				expect(chars::validate<chars::CharsCategory::UTF8_CHAR>(source) == "valid utf8_char string"_b) << fatal;
 				expect(chars::length<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF8>(source) == value{source.size()}) << fatal;
-				expect(chars::validate<chars::CharsCategory::UTF8>(chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF8>(source)) == "valid utf8 string"_b) << fatal;
-				expect(chars::validate<chars::CharsCategory::UTF8>(chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF8, chars::InputProcessPolicy::ASSUME_VALID_INPUT>(source)) == "valid utf8 string"_b) << fatal;
+
+				const auto dest = chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF8>(source);
+				expect(chars::validate<chars::CharsCategory::UTF8>(dest) == "valid utf8 string"_b) << fatal;
+
+				const auto dest_a = chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF8, chars::InputProcessPolicy::ASSUME_VALID_INPUT>(source);
+				expect(chars::validate<chars::CharsCategory::UTF8>(dest_a) == "valid utf8 string"_b) << fatal;
 			};
-			
-			// "to_utf16_le"_test = [source = make_source<char>(generator, random.get<std::size_t>(std::numeric_limits<char>::max() / 2, std::numeric_limits<char>::max()))]
-			// {
-			// 	using operators::operator==;
-			// 	expect(chars::validate<chars::CharsCategory::UTF8_CHAR>(source) == "valid utf8_char string"_b) << fatal;
-			// 	expect(chars::length<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF16_LE>(source) == value{source.size()}) << fatal;
-			// 	expect(chars::validate<chars::CharsCategory::UTF16_LE>(chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF16_LE>(source)) == "valid utf16_le string"_b) << fatal;
-			// 	// expect(chars::validate<chars::CharsCategory::UTF16_LE>(chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF16_LE, chars::InputProcessPolicy::ASSUME_VALID_INPUT>(source)) == "valid utf16_le string"_b) << fatal;
-			// };
-			//
-			// "to_utf16_be"_test = [source = make_source<char>(generator, random.get<std::size_t>(std::numeric_limits<char>::max() / 2, std::numeric_limits<char>::max()))]
-			// {
-			// 	using operators::operator==;
-			// 	expect(chars::validate<chars::CharsCategory::UTF8_CHAR>(source) == "valid utf8_char string"_b) << fatal;
-			// 	expect(chars::length<chars::CharsCategory::ASCII, chars::CharsCategory::UTF16_BE>(source) == value{source.size()}) << fatal;
-			// 	expect(chars::validate<chars::CharsCategory::UTF16_BE>(chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF16_BE>(source)) == "valid utf16_be string"_b) << fatal;
-			// 	// expect(chars::validate<chars::CharsCategory::UTF16_BE>(chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF16_BE, chars::InputProcessPolicy::ASSUME_VALID_INPUT>(source)) == "valid utf16_be string"_b) << fatal;
-			// };
-			//
-			// "to_utf32"_test = [source = make_source<char>(generator, random.get<std::size_t>(std::numeric_limits<char>::max() / 2, std::numeric_limits<char>::max()))]
-			// {
-			// 	using operators::operator==;
-			// 	expect(chars::validate<chars::CharsCategory::UTF8_CHAR>(source) == "valid utf8_char string"_b) << fatal;
-			// 	expect(chars::length<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF32>(source) == value{source.size()}) << fatal;
-			// 	expect(chars::validate<chars::CharsCategory::UTF32>(chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF32>(source)) == "valid utf32 string"_b) << fatal;
-			// 	// expect(chars::validate<chars::CharsCategory::UTF32>(chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF32, chars::InputProcessPolicy::ASSUME_VALID_INPUT>(source)) == "valid utf32 string"_b) << fatal;
-			// };
+
+			"to_utf16_le"_test = [source = make_source<char>(generator, random.get<std::size_t>(std::numeric_limits<char>::max() / 2, std::numeric_limits<char>::max()))]
+			{
+				using operators::operator==;
+				expect(chars::validate<chars::CharsCategory::UTF8_CHAR>(source) == "valid utf8_char string"_b) << fatal;
+
+				const auto dest = chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF16_LE>(source);
+				expect(chars::validate<chars::CharsCategory::UTF16_LE>(dest) == "valid utf16_le string"_b) << fatal;
+
+				const auto dest_a = chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF16_LE, chars::InputProcessPolicy::ASSUME_VALID_INPUT>(source);
+				expect(chars::validate<chars::CharsCategory::UTF16_LE>(dest_a) == "valid utf16_le string"_b) << fatal;
+			};
+
+			"to_utf16_be"_test = [source = make_source<char>(generator, random.get<std::size_t>(std::numeric_limits<char>::max() / 2, std::numeric_limits<char>::max()))]
+			{
+				using operators::operator==;
+				expect(chars::validate<chars::CharsCategory::UTF8_CHAR>(source) == "valid utf8_char string"_b) << fatal;
+
+				const auto dest = chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF16_BE>(source);
+				expect(chars::validate<chars::CharsCategory::UTF16_BE>(dest) == "valid utf16_be string"_b) << fatal;
+
+				const auto dest_a = chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF16_BE, chars::InputProcessPolicy::ASSUME_VALID_INPUT>(source);
+				expect(chars::validate<chars::CharsCategory::UTF16_BE>(dest_a) == "valid utf16_be string"_b) << fatal;
+			};
+
+			"to_utf32"_test = [source = make_source<char>(generator, random.get<std::size_t>(std::numeric_limits<char>::max() / 2, std::numeric_limits<char>::max()))]
+			{
+				using operators::operator==;
+				expect(chars::validate<chars::CharsCategory::UTF8_CHAR>(source) == "valid utf8_char string"_b) << fatal;
+
+				const auto dest = chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF32>(source);
+				expect(chars::validate<chars::CharsCategory::UTF32>(dest) == "valid utf32 string"_b) << fatal;
+
+				const auto dest_a = chars::convert<chars::CharsCategory::UTF8_CHAR, chars::CharsCategory::UTF32, chars::InputProcessPolicy::ASSUME_VALID_INPUT>(source);
+				expect(chars::validate<chars::CharsCategory::UTF32>(dest_a) == "valid utf32 string"_b) << fatal;
+			};
+		}
+
+		config().output_level = old_level;
+	};
+
+	GAL_PROMETHEUS_COMPILER_NO_DESTROY unit_test::suite<"chars.utf8"> chars_utf8 = []
+	{
+		using namespace unit_test;
+		using namespace literals;
+
+		const auto old_level = std::exchange(config().output_level, OutputLevel::NONE);
+
+		numeric::Random<numeric::RandomStateCategory::PRIVATE, numeric::random_engine_xrsr_128_plus_plus> random;
+		const auto generator = [&random](auto& source) mutable -> std::size_t
+		{
+			unsigned v;
+			do
+			{
+				v = random.get<unsigned>(0, 0x0010'ffef);
+			} while (
+				// skip surrogate pairs
+				(v >= 0xd800 and v <= 0xdfff) // or
+				// // skip non-characters
+				// (v >= 0xfdd0 and v <= 0xfdef) or (v & 0xfffe) == 0xfffe or
+				// // skip control characters
+				// (v <= 0x001f) or (v >= 0x007f and v <= 0x009f) or
+				// // skip special characters
+				// (v >= 0xfff0 and v <= 0xfff8) or
+				// // skip tag characters and variation selector supplement
+				// (v >= 0x000e'0000 and v <= 0x000e'01ef) or
+				// // skip private use areas
+				// (v >= 0x000f'0000) or (v >= 0xe000 and v <= 0xf8ff) or
+				// // skip ideographic description characters
+				// (v >= 0x2ff0 and v <= 0x2fff)
+			);
+
+			if (v < 0x0080)
+			{
+				source.push_back(static_cast<char8_t>(v));
+				return 1;
+			}
+			else if (v < 0x0800)
+			{
+				source.push_back(static_cast<char8_t>(0xc0 | (v >> 6)));
+				source.push_back(static_cast<char8_t>(0x80 | (v & 0x3f)));
+				return 2;
+			}
+			else if (v < 0x0001'0000)
+			{
+				source.push_back(static_cast<char8_t>(0xe0 | (v >> 12)));
+				source.push_back(static_cast<char8_t>(0x80 | ((v >> 6) & 0x3f)));
+				source.push_back(static_cast<char8_t>(0x80 | (v & 0x3f)));
+				return 3;
+			}
+			else
+			{
+				source.push_back(static_cast<char8_t>(0xf0 | (v >> 18)));
+				source.push_back(static_cast<char8_t>(0x80 | ((v >> 12) & 0x3f)));
+				source.push_back(static_cast<char8_t>(0x80 | ((v >> 6) & 0x3f)));
+				source.push_back(static_cast<char8_t>(0x80 | (v & 0x3f)));
+				return 4;
+			}
+		};
+		const auto generator_char_only = [&random](auto& source) mutable -> std::size_t
+		{
+			source.push_back(static_cast<char8_t>(random.get<unsigned>() & std::numeric_limits<char>::max()));
+			return 1;
+		};
+
+		constexpr std::size_t trials = 1000;
+		for (std::size_t i = 0; i < trials; ++i)
+		{
+			"to_ascii"_test = [source = make_source<char8_t>(generator_char_only, random.get<std::size_t>(std::numeric_limits<char8_t>::max() / 2, std::numeric_limits<char8_t>::max()))]
+			{
+				using operators::operator==;
+				expect(chars::validate<chars::CharsCategory::UTF8>(source) == "valid utf8 string"_b) << fatal;
+				// fixme: MSVC ICE HERE!
+				// expect(chars::length<chars::CharsCategory::UTF8, chars::CharsCategory::ASCII>(source) == value{source.size()}) << fatal;
+
+				const auto dest = chars::convert<chars::CharsCategory::UTF8, chars::CharsCategory::ASCII>(source);
+				expect(chars::validate<chars::CharsCategory::ASCII>(dest) == "valid ascii string"_b) << fatal;
+
+				const auto dest_a = chars::convert<chars::CharsCategory::UTF8, chars::CharsCategory::ASCII, chars::InputProcessPolicy::ASSUME_VALID_INPUT>(source);
+				expect(chars::validate<chars::CharsCategory::ASCII>(dest_a) == "valid ascii string"_b) << fatal;
+			};
+
+			"to_utf8_char"_test = [source = make_source<char8_t>(generator, random.get<std::size_t>(std::numeric_limits<char8_t>::max() / 2, std::numeric_limits<char8_t>::max()))]
+			{
+				using operators::operator==;
+				expect(chars::validate<chars::CharsCategory::UTF8>(source) == "valid utf8 string"_b) << fatal;
+				// fixme: MSVC ICE HERE!
+				// expect(chars::length<chars::CharsCategory::UTF8, chars::CharsCategory::UTF8_CHAR>(source) == value{source.size()}) << fatal;
+
+				const auto dest = chars::convert<chars::CharsCategory::UTF8, chars::CharsCategory::UTF8_CHAR>(source);
+				expect(chars::validate<chars::CharsCategory::UTF8_CHAR>(dest) == "valid utf8_char string"_b) << fatal;
+
+				const auto dest_a = chars::convert<chars::CharsCategory::UTF8, chars::CharsCategory::UTF8_CHAR, chars::InputProcessPolicy::ASSUME_VALID_INPUT>(source);
+				expect(chars::validate<chars::CharsCategory::UTF8_CHAR>(dest_a) == "valid utf8_char string"_b) << fatal;
+			};
+
+			"to_utf8"_test = [source = make_source<char8_t>(generator, random.get<std::size_t>(std::numeric_limits<char8_t>::max() / 2, std::numeric_limits<char8_t>::max()))]
+			{
+				using operators::operator==;
+				expect(chars::validate<chars::CharsCategory::UTF8>(source) == "valid utf8 string"_b) << fatal;
+				// fixme: MSVC ICE HERE!
+				// expect(chars::length<chars::CharsCategory::UTF8, chars::CharsCategory::UTF8>(source) == value{source.size()}) << fatal;
+
+				const auto dest = chars::convert<chars::CharsCategory::UTF8, chars::CharsCategory::UTF8>(source);
+				expect((dest == ref{source}) == "valid utf8 string"_b) << fatal;
+
+				const auto dest_a = chars::convert<chars::CharsCategory::UTF8, chars::CharsCategory::UTF8, chars::InputProcessPolicy::ASSUME_VALID_INPUT>(source);
+				expect((dest_a == ref{source}) == "valid utf8 string"_b) << fatal;
+			};
+
+			"to_utf16_le"_test = [source = make_source<char8_t>(generator, random.get<std::size_t>(std::numeric_limits<char8_t>::max() / 2, std::numeric_limits<char8_t>::max()))]
+			{
+				using operators::operator==;
+				expect(chars::validate<chars::CharsCategory::UTF8>(source) == "valid utf8 string"_b) << fatal;
+
+				const auto dest = chars::convert<chars::CharsCategory::UTF8, chars::CharsCategory::UTF16_LE>(source);
+				expect(chars::validate<chars::CharsCategory::UTF16_LE>(dest) == "valid utf16_le string"_b) << fatal;
+
+				const auto dest_a = chars::convert<chars::CharsCategory::UTF8, chars::CharsCategory::UTF16_LE, chars::InputProcessPolicy::ASSUME_VALID_INPUT>(source);
+				expect(chars::validate<chars::CharsCategory::UTF16_LE>(dest_a) == "valid utf16_le string"_b) << fatal;
+			};
+
+			"to_utf16_be"_test = [source = make_source<char8_t>(generator, random.get<std::size_t>(std::numeric_limits<char8_t>::max() / 2, std::numeric_limits<char8_t>::max()))]
+			{
+				using operators::operator==;
+				expect(chars::validate<chars::CharsCategory::UTF8>(source) == "valid utf8 string"_b) << fatal;
+
+				const auto dest = chars::convert<chars::CharsCategory::UTF8, chars::CharsCategory::UTF16_BE>(source);
+				expect(chars::validate<chars::CharsCategory::UTF16_BE>(dest) == "valid utf16_be string"_b) << fatal;
+
+				const auto dest_a = chars::convert<chars::CharsCategory::UTF8, chars::CharsCategory::UTF16_BE, chars::InputProcessPolicy::ASSUME_VALID_INPUT>(source);
+				expect(chars::validate<chars::CharsCategory::UTF16_BE>(dest_a) == "valid utf16_be string"_b) << fatal;
+			};
+
+			"to_utf32"_test = [source = make_source<char8_t>(generator, random.get<std::size_t>(std::numeric_limits<char8_t>::max() / 2, std::numeric_limits<char8_t>::max()))]
+			{
+				using operators::operator==;
+				expect(chars::validate<chars::CharsCategory::UTF8>(source) == "valid utf8 string"_b) << fatal;
+
+				const auto dest = chars::convert<chars::CharsCategory::UTF8, chars::CharsCategory::UTF32>(source);
+				expect(chars::validate<chars::CharsCategory::UTF32>(dest) == "valid utf32 string"_b) << fatal;
+
+				const auto dest_a = chars::convert<chars::CharsCategory::UTF8, chars::CharsCategory::UTF32, chars::InputProcessPolicy::ASSUME_VALID_INPUT>(source);
+				expect(chars::validate<chars::CharsCategory::UTF32>(dest_a) == "valid utf32 string"_b) << fatal;
+			};
 		}
 
 		config().output_level = old_level;
