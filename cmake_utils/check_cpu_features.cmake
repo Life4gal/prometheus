002321b23741c9d42379f6626829ee0871e94a1a
life4gal <life4gal@gmail.com>
2024-07-18 13:46:39 +0800

    `build`: Fixed bug where `COMPILE_DEFINITIONS` was not set correctly when using `try_run` to test CPU features, causing it to always fail.

--- cmake_utils/check_cpu_features.cmake
+++ cmake_utils/check_cpu_features.cmake
@@ -4,10 +4,16 @@ function(check_cpu_features feature_name)
 	try_run(
 		RUN_RESULT COMPILE_RESULT
 		SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/cmake_utils/check_cpu_features_${feature_name}.cpp
-		COMPILE_DEFINITIONS 
-		"${${PROJECT_NAME_PREFIX}PLATFORM}"
+
+		COMPILE_DEFINITIONS "-D${${PROJECT_NAME_PREFIX}PLATFORM}"
+		COMPILE_OUTPUT_VARIABLE COMPILE_OUTPUT_RESULT
 		CXX_STANDARD 23
+		CXX_STANDARD_REQUIRED true
 	)
+
+	if (NOT COMPILE_RESULT)
+		message(FATAL_ERROR "[check_cpu_features] build failed: \n${COMPILE_OUTPUT_RESULT}")
+	endif (NOT COMPILE_RESULT)
 	
 	if (RUN_RESULT EQUAL 1)
 		message(STATUS "[check_cpu_features] '${upper_feature_name}' instruction sets are supported.")
