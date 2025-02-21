57b562f9d8d54b7db0bab72bf498c18053efac14
life4gal <life4gal@gmail.com>
2025-02-21 16:05:13 +0800

    `test`: Test suite/test names support wildcards.🚨

--- unit_test/src/main.cpp
+++ unit_test/src/main.cpp
@@ -1,8 +1,6 @@
 #include <command_line_parser/command_line_parser.hpp>
 #include <unit_test/unit_test.hpp>
 
-#include <chars/chars.hpp>
-
 using namespace gal::prometheus;
 
 int main(const int, char*[])
@@ -21,14 +19,12 @@ int main(const int, char*[])
 			.add_option("call-debugger-if-fail", option_type::default_value("false"))
 			.add_option("call-debugger-if-fatal", option_type::default_value("true"))
 			//
-			;
-	parser
 			.add_alias("x-fail", "call-debugger-if-fail")
 			.add_alias("x-fatal", "call-debugger-if-fatal")
 			//
 			;
 
-	auto& config = unit_test::config();
+	unit_test::config_type config{};
 
 	try
 	{
@@ -44,8 +40,6 @@ int main(const int, char*[])
 	const auto tab_width = parser["tab-width"].as<std::size_t>();
 	const auto max_failures = parser["max-failures"].as<std::size_t>();
 	const auto dry_run = parser["dry-run"].as<bool>();
-	auto exec_suite_name = parser["exec-suite-name"].as<std::vector<std::string>>();
-	auto exec_test_name = parser["exec-test-name"].as<std::vector<std::string>>();
 	const auto call_debugger_if_fail = parser["call-debugger-if-fail"].as<bool>();
 	const auto call_debugger_if_fatal = parser["call-debugger-if-fatal"].as<bool>();
 
@@ -61,44 +55,82 @@ int main(const int, char*[])
 	{
 		config.dry_run = *dry_run;
 	}
-	if (exec_suite_name.has_value())
-	{
-		config.filter_execute_suite_name =
-				[suite = std::move(*exec_suite_name)](const auto& name) noexcept -> bool
-				{
-					return std::ranges::contains(suite, name);
-				};
-	}
-	if (exec_test_name.has_value())
-	{
-		config.filter_execute_test_name =
-				[test = std::move(*exec_test_name)](const auto& name) noexcept -> bool
-				{
-					return std::ranges::contains(test, name);
-				};
-	}
 	if (call_debugger_if_fail.has_value())
 	{
 		if (*call_debugger_if_fail)
 		{
-			config.debug_break_point |= unit_test::DebugBreakPoint::FAIL;
+			config.break_point_level |= unit_test::config_type::BreakPointLevel::FAILURE;
 		}
 		else
 		{
-			config.debug_break_point &= ~unit_test::DebugBreakPoint::FAIL;
+			config.break_point_level &= ~unit_test::config_type::BreakPointLevel::FAILURE;
 		}
 	}
 	if (call_debugger_if_fatal.has_value())
 	{
 		if (*call_debugger_if_fatal)
 		{
-			config.debug_break_point |= unit_test::DebugBreakPoint::FATAL;
+			config.break_point_level |= unit_test::config_type::BreakPointLevel::FATAL;
 		}
 		else
 		{
-			config.debug_break_point &= ~unit_test::DebugBreakPoint::FATAL;
+			config.break_point_level &= ~unit_test::config_type::BreakPointLevel::FATAL;
 		}
 	}
+
+	if (const auto& option = parser["exec-suite-name"];
+		option.set())
+	{
+		if (const auto wildcard = option.as<std::string_view>();
+			wildcard.has_value() and *wildcard == "*")
+		{
+			config.filter_suite = [](const unit_test::suite_node_type& node) noexcept -> bool
+			{
+				std::ignore = node;
+				return true;
+			};
+		}
+		else if (auto suite_names = option.as<std::vector<std::string>>();
+			suite_names.has_value())
+		{
+			config.filter_suite =
+					[suite = std::move(*suite_names)](const unit_test::suite_node_type& node) noexcept -> bool
+					{
+						return std::ranges::contains(suite, node.name);
+					};
+		}
+	}
+
+	if (const auto& option = parser["exec-test-name"];
+		option.set())
+	{
+		if (const auto wildcard = option.as<std::string_view>();
+			wildcard.has_value() and *wildcard == "*")
+		{
+			config.filter_test = [](const unit_test::test_node_type& node) noexcept -> bool
+			{
+				std::ignore = node;
+				return true;
+			};
+		}
+		else if (auto test_names = option.as<std::vector<std::string>>();
+			test_names.has_value())
+		{
+			config.filter_test =
+					[test = std::move(*test_names)](const unit_test::test_node_type& node) noexcept -> bool
+					{
+						if (node.parent != nullptr)
+						{
+							// child
+							return true;
+						}
+
+						return std::ranges::contains(test, node.name) and not std::ranges::contains(node.categories.get(), "skip");
+					};
+		}
+	}
+
+	set_config(std::move(config));
 
 	return 0;
 }
