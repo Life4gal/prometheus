5c96e170b2a5666ceeeeb4286783a2220802d141
life4gal <life4gal@gmail.com>
2025-03-11 13:37:50 +0800

    `feat`: Use pthread.

--- src/concurrency/thread.cpp
+++ src/concurrency/thread.cpp
@@ -15,8 +15,26 @@
 #include GAL_PROMETHEUS_ERROR_DEBUG_MODULE
 
 #if defined(GAL_PROMETHEUS_PLATFORM_WINDOWS)
+
 #include <Windows.h>
+
+#elif defined(GAL_PROMETHEUS_PLATFORM_LINUX)
+
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+// this_thread::get_id ==> syscall(SYS_gettid)
+// #include <sys/syscall.h>
+
+#elif defined(GAL_PROMETHEUS_PLATFORM_DARWIN)
+
+#include <pthread.h>
+
 #else
+
+#error "Unsupported platform"
+
 #endif
 
 namespace
@@ -34,6 +52,7 @@ namespace gal::prometheus::concurrency
 		[[nodiscard]] auto get_id() noexcept -> process_id
 		{
 			#if defined(GAL_PROMETHEUS_PLATFORM_WINDOWS)
+
 			// return GetCurrentProcessId();
 
 			#if defined(_WIN64)
@@ -46,8 +65,14 @@ namespace gal::prometheus::concurrency
             return __readfsdword(nt_thread_information_block_current_process_id);
 			#endif
 
+			#elif defined(GAL_PROMETHEUS_PLATFORM_LINUX) or defined(GAL_PROMETHEUS_PLATFORM_DARWIN)
+
+			return static_cast<process_id>(getpid());
+
 			#else
+
             GAL_PROMETHEUS_ERROR_DEBUG_UNREACHABLE();
+
 			#endif
 		}
 	}
@@ -57,6 +82,7 @@ namespace gal::prometheus::concurrency
 		[[nodiscard]] auto get_id() noexcept -> thread_id
 		{
 			#if defined(GAL_PROMETHEUS_PLATFORM_WINDOWS)
+
 			// return GetCurrentThreadId();
 
 			#if defined(_WIN64)
@@ -69,33 +95,80 @@ namespace gal::prometheus::concurrency
             return __readfsdword(nt_thread_information_block_current_thread_id);
 			#endif
 
+			#elif defined(GAL_PROMETHEUS_PLATFORM_LINUX)
+
+			return static_cast<thread_id>(pthread_self());
+
+			#elif defined(GAL_PROMETHEUS_PLATFORM_DARWIN)
+
+			uint64_t tid;
+			pthread_threadid_np(nullptr, &tid);
+			return static_cast<thread_id>(tid);
+
 			#else
+
             GAL_PROMETHEUS_ERROR_DEBUG_UNREACHABLE();
+
 			#endif
 		}
 
 		auto set_name(const std::string_view name) noexcept -> void
 		{
+			const auto id = get_id();
+
 			#if defined(GAL_PROMETHEUS_PLATFORM_WINDOWS)
-			// fixme
-			const auto n = std::filesystem::path{name};
-			const auto result = SetThreadDescription(GetCurrentThread(), n.c_str());
+
+			// fixme: UTF8 => UTF16
+			const auto path = std::filesystem::path{name};
+			const auto real_name = path.string();
+
+			const auto result = SetThreadDescription(GetCurrentThread(), path.c_str());
 			GAL_PROMETHEUS_ERROR_DEBUG_ASSUME(SUCCEEDED(result));
 
-			const auto lock = std::scoped_lock{thread_names_mutex};
-			const auto id = get_id();
-			if (const auto it = thread_names.find(id);
-				it != thread_names.end()) { it->second = n.string(); }
-			else { thread_names.emplace_hint(it, id, n.string()); }
+			#elif defined(GAL_PROMETHEUS_PLATFORM_LINUX)
+
+			// fixme: truncated?
+			const auto real_name = name;
+
+			const auto result = pthread_setname_np(id, name.data());
+			GAL_PROMETHEUS_ERROR_DEBUG_ASSUME(result == 0);
+
+			#elif defined(GAL_PROMETHEUS_PLATFORM_DARWIN)
+
+			// fixme: truncated?
+			const auto real_name = name;
+
+			const auto result = pthread_setname_np(name.data());
+			GAL_PROMETHEUS_ERROR_DEBUG_ASSUME(result == 0);
+
 			#else
+
             GAL_PROMETHEUS_ERROR_DEBUG_UNREACHABLE();
+
 			#endif
+
+			const auto lock = std::scoped_lock{thread_names_mutex};
+
+			if (const auto it = thread_names.find(id);
+				it != thread_names.end())
+			{
+				it->second = real_name;
+			}
+			else
+			{
+				thread_names.emplace_hint(it, id, real_name);
+			}
 		}
 
 		auto get_name(const thread_id id) noexcept -> std::optional<std::string_view>
 		{
 			const auto lock = std::scoped_lock{thread_names_mutex};
-			if (const auto it = thread_names.find(id); it != thread_names.end()) { return it->second; }
+
+			if (const auto it = thread_names.find(id);
+				it != thread_names.end())
+			{
+				return it->second;
+			}
 
 			return std::nullopt;
 		}
